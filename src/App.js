918471907124db509879cfe946c2b60069b08762
shyamkumar-7 <130834063+shyamkumar-7@users.noreply.github.com>
2023-09-21 16:45:51 +0530

    Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import data from './data'
+import List from './list'
+function App() {
+  const [people, setPeople] = useState(data)
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} Birthdays today</h3>
+        <List people={people} />
+        <button className='btn' onClick={() => setPeople([])}>Clear all</button>
+      </section>
+    </main>
+  )
+}
+
+export default App

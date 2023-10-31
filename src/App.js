9d6ca3ca38197c771c68c24e2fed7a4cecfdbb7b
shyamkumar-7 <130834063+shyamkumar-7@users.noreply.github.com>
2023-10-31 22:24:01 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import data from './data'
 import List from './list'
 function App() {
-  const [people, setPeople] = useState(data)
+  const [people, setPeople] = useState(data);
   return (
     <main>
       <section className='container'>
--- src/data.js
+++ src/data.js
@@ -1,37 +1,39 @@
+import Sinchan from './images/sinchan.png';
+import rengoku from './images/rengoku.jpeg';
+import muzan from './images/muzan.jpeg';
+import tengen from './images/tengen.jpeg';
+import L from './images/L.jpeg'
+
 export default [
     {
       id: 1,
-      name: 'Bertie Yates',
-      age: 29,
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+      name: 'Sinchan Nohara',
+      age: 5,
+      image : Sinchan,
     },
+    // https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg
     {
       id: 2,
-      name: 'Hester Hogan',
-      age: 32,
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+      name: 'Kyojuro Rengoku',
+      age: 20,
+      image: rengoku,
     },
     {
       id: 3,
-      name: 'Larry Little',
-      age: 36,
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      name: 'Tengen Uzui',
+      age: 23,
+      image: tengen,
     },
     {
       id: 4,
-      name: 'Sean Walsh',
-      age: 34,
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      name: 'Muzan Kibutsuji',
+      age: 1000,
+      image: muzan,
     },
     {
       id: 5,
-      name: 'Lola Gardner',
-      age: 29,
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      name: 'L Lawliet',
+      age: 24,
+      image: L,
     },
   ];
